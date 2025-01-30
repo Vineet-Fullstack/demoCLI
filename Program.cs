2ca7a886c4d00aa59abc5144aa0a524bcef85e58
Vineet <vineet.fullstack.developer@gmail.com>
2025-01-30 11:02:35 +0530

    initial push

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace demoCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Demo CLI Tool\nUsage: democli <command>");
+                return;
+            }
+
+            string command = args[0].ToLower();
+
+            switch (command)
+            {
+                case "hello":
+                    Console.WriteLine("Hello, welcome to Demo CLI!");
+                    break;
+                case "version":
+                    Console.WriteLine("Demo CLI Tool v1.0.0");
+                    break;
+                default:
+                    Console.WriteLine($"Unknown command: {command}");
+                    break;
+            }
+        }
+    }
+}
